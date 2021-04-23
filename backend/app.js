e97411bceed02fa060a4271fa7e1d361e933a019
Vlad-maker <skateislife@mail.ru>
2021-04-23 20:17:25 +0300

    Feat: Remote Server

--- backend/app.js
+++ backend/app.js
@@ -26,11 +26,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(cors())
 app.use(bodyParser.json());
 app.use(requestLogger);
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post(
   '/signin',
